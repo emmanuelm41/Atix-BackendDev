f75252e8f5654035e4e157cceda26ce4a24436d7
Emmanuel <40460347+emmanuelm42@users.noreply.github.com>
2020-09-21 10:57:37 -0300

    create worker class to allow concurrency

---
+++ 1_Coding/Hashing/src/models/Worker.ts
@@ -0,0 +1,84 @@
+import * as uuid from "uuid";
+import { Line } from "./Line";
+import { Logger } from "../models/Logger";
+
+interface GenericObject {
+    [key: string]: any;
+}
+
+const context: { lastHash: string; lines: string[] } = {
+    lastHash: "0000000000000000000000000000000000000000000000000000000000000000",
+    lines: [],
+};
+
+class Worker {
+    isRunning: boolean = false;
+    tasks: { id: string; payload: GenericObject }[] = [];
+    events: { [key: string]: () => void } = {};
+
+    constructor(interval: number) {
+        setInterval(() => {
+            const logger = Logger.getInstance();
+
+            if (!this.isRunning) {
+                while (this.tasks.length) {
+                    const { id, payload } = this.tasks.shift() || {};
+
+                    if (id && payload) {
+                        logger.trace(`[${id}] - Starting task`);
+                        this.runTask(id, payload);
+                        logger.trace(`[${id}] - Task finished`);
+
+                        this.trigger(id);
+                    }
+                }
+            }
+        }, interval);
+    }
+
+    runTask(id: string, payload: GenericObject) {
+        const logger = Logger.getInstance();
+        const body = JSON.stringify(payload);
+
+        const prevHash = context.lastHash;
+        const { hash, nonce } = Line.getNew(prevHash, body);
+        const newLine = `${prevHash},${body},${nonce}`;
+
+        context.lines.push(newLine);
+        context.lastHash = hash;
+
+        logger.trace(`[${id}] - Output file: ${JSON.stringify(context)}`);
+
+        return newLine;
+    }
+
+    addTask(payload: GenericObject) {
+        const logger = Logger.getInstance();
+        const id = uuid.v1();
+        logger.trace(`[${id}] - Adding new task to the queu`);
+
+        this.tasks.push({ id, payload });
+
+        return id;
+    }
+
+    trigger(id: string) {
+        if (this.events[id]) {
+            this.events[id]();
+            delete this.events[id];
+        }
+    }
+
+    on(id: string, callback: () => void) {
+        this.events[id] = callback;
+    }
+}
+
+export class WorkerInstance {
+    static instance: Worker;
+
+    static getInstance() {
+        if (!this.instance) this.instance = new Worker(100);
+        return this.instance;
+    }
+}
--- 1_Coding/Hashing/src/routes/addLine.ts
+++ 1_Coding/Hashing/src/routes/addLine.ts
@@ -1,13 +1,6 @@
-import { FastifyReply, FastifyRequest, RawRequestDefaultExpression, RawServerBase, RawServerDefault, RouteOptions } from "fastify";
-import * as uuid from "uuid";
-
+import { FastifyReply, FastifyRequest, RouteOptions } from "fastify";
 import { Logger } from "../models/Logger";
-import { Line } from "../models/Line";
-
-const context: { lastHash: string; lines: string[] } = {
-    lastHash: "0000000000000000000000000000000000000000000000000000000000000000",
-    lines: [],
-};
+import { WorkerInstance } from "../models/Worker";
 
 export const newLineRouter: RouteOptions = {
     method: "POST",
@@ -25,24 +18,15 @@ export const newLineRouter: RouteOptions = {
         },
     },
     // this function is executed for every request before the handler is executed
-    preHandler: async (request: FastifyRequest, reply: FastifyReply) => {
-        const __id = uuid.v1();
-
-        Logger.getInstance().trace(`[${__id}] - New request rcv: ${JSON.stringify(request.body)}`);
-    },
+    preHandler: async (request: FastifyRequest, reply: FastifyReply) => {},
     handler: async (request: FastifyRequest, reply: FastifyReply) => {
         const logger = Logger.getInstance();
-        const body = JSON.stringify(request.body);
-
-        const prevHash = context.lastHash;
-        const { hash, nonce } = Line.getNew(prevHash, body);
-
-        context.lines.push(`${prevHash},${body},${nonce}`);
-        context.lastHash = hash;
-
         reply.header("Access-Control-Allow-Origin", "*");
 
-        logger.trace(`Output file: ${JSON.stringify(context)}`);
-        return { response_code: -1, response_message: "OK" };
+        const id = WorkerInstance.getInstance().addTask(request.body as { [key: string]: any });
+        WorkerInstance.getInstance().on(id, () => {
+            logger.trace(`[${id}] - Sending response`);
+            reply.status(200).send({ response_code: -1, response_message: "OK" });
+        });
     },
 };
