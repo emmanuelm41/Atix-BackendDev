514682b5fb5a336f5cbb4c414553a915e0ca99dc
Emmanuel <40460347+emmanuelm42@users.noreply.github.com>
2020-09-21 21:23:18 -0300

    handle http server error

--- 1_Coding/Hashing/src/index.ts
+++ 1_Coding/Hashing/src/index.ts
@@ -1,6 +1,13 @@
 import { Server } from "./models/Server";
+import { Logger } from "./models/Logger";
 import { newLineRouter } from "./routes/addLine";
 
+const logger = Logger.getInstance();
 const server = new Server();
 server.addRoute(newLineRouter);
-server.start(3000);
+
+try {
+    server.start(3000);
+} catch (error) {
+    logger.fatal("We could not start HTTP server");
+}
