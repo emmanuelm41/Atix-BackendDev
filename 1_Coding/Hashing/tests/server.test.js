860a3b9acd18d84ff1d7294242c889ec7061f85a
Emmanuel <40460347+emmanuelm42@users.noreply.github.com>
2020-09-21 21:37:41 -0300

    add new test case

--- 1_Coding/Hashing/tests/server.test.js
+++ 1_Coding/Hashing/tests/server.test.js
@@ -1,4 +1,8 @@
 const axios = require("axios");
+const fs = require("fs");
+const parseCsv = require("csv-parse/lib/sync");
+
+const { Line } = require("../build/models/Line");
 
 test("Post a new message", async () => {
     const message = { test: new Date() };
@@ -7,6 +11,36 @@ test("Post a new message", async () => {
         const response = await axios.post(`http://localhost:3000/`, message);
         expect(response.status).toBe(200);
     } catch (error) {
+        expect(400).toBe(200);
+    }
+});
+
+test("Test source integrity", async () => {
+    const message = { test: new Date() };
+
+    try {
+        let response;
+
+        response = await axios.post(`http://localhost:3000/`, message);
+        expect(response.status).toBe(200);
+        response = await axios.post(`http://localhost:3000/`, message);
+        expect(response.status).toBe(200);
+        response = await axios.post(`http://localhost:3000/`, message);
+        expect(response.status).toBe(200);
+        response = await axios.post(`http://localhost:3000/`, message);
+        expect(response.status).toBe(200);
+        response = await axios.post(`http://localhost:3000/`, message);
+        expect(response.status).toBe(200);
+        response = await axios.post(`http://localhost:3000/`, message);
+        expect(response.status).toBe(200);
+
+        const csv = parseCsv(fs.readFileSync("./source/file.csv"), { columns: false, quote: false });
+        csv.forEach(([hash, payload, nonce]) => {
+            const isValid = Line.checkIntegrity(hash, payload, nonce);
+            expect(isValid).toBeTruthy();
+        });
+    } catch (error) {
+        console.log(error);
         expect(400).toBe(200);
     }
 });
